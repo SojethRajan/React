cded3ac5214a1b969f453a1903dc121ab1ef2fdd
SojethRajan <sojetha@gmail.com>
2023-12-27 22:06:58 +0530

    Pushed changes for assignment 1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('gjvsdjjsg')
+const container = React.createElement(
+    'div',
+    {
+        id : 'contaier'
+    },
+    'hello div'
+)
+const heading1 = React.createElement(
+    'h1',
+    {
+        id : 'title',
+        className : 'classname'
+    },
+    'Hello React'
+    );
+
+const heading2 = React.createElement(
+    'p',
+    {
+        id : 'title'
+    },
+    'Hello heading2'
+    );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render([container,heading1,heading2]);
