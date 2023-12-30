98141b8701abf1f77d8eb29699ca63edeeb2681f
SojethRajan <sojetha@gmail.com>
2023-12-30 20:18:01 +0530

    Practised from lecture 3

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";          
 
-const heading1 = React.createElement(
-    'h1',
-    {
-        id : 'title',
-        className : 'classname'
-    },
-    'Hello React'
-    );
+const Header = () => {
+    return(
+        <div>
+            <h1>This is the Header</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Help</li>
+            </ul>
+        </div>
+    )
+}
 
-const paragraph = React.createElement(
-    'p',
-    {
-        id : 'title'
-    },
-    'Hello paragraph'
-    );
-
-
-const container = React.createElement(
-    'div',
-    {
-        id : 'contaier'
-    },
-    [heading1,paragraph]
+const Footer = () => (
+    <h1>This is the Footer</h1>
 )
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(container);
 
-console.log('Created a feature branch')
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render([<Header/>, <Footer/>]);
+
